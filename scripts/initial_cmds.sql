5be73152dac30b9901f8626d9fb73be9d5469e9a
susmithayoga21 <susmithakokila21@gmail.com>
2026-04-02 11:36:53 +0800

    Add files via upload

---
+++ scripts/initial_cmds.sql
@@ -0,0 +1,14 @@
+Create DATABASE SwiggyDB -- Creating Swiggy DataBase
+
+USE SwiggyDB
+
+CREATE SCHEMA bronze;
+GO
+
+-- Create Schemas
+CREATE SCHEMA silver;
+GO
+CREATE SCHEMA gold;
+GO
+DROP SCHEMA silver;
+GO
